d7e085392b25b6ca8aab43ab04dfbbb56a611638
Simon Oulevay <simon.oulevay@gmail.com>
2015-07-31 16:17:43 +0200

    Formatter specs.

--- lib/probe_dock_cucumber/formatter.rb
+++ lib/probe_dock_cucumber/formatter.rb
@@ -1,74 +1,150 @@
 require 'paint'
 require 'digest/sha1'
 
+# Cucumber formatter to send test results to Probe Dock.
+#
+# The following events can be received by a formatter:
+#
+# before_features
+#   before_feature
+#     before_comment
+#       comment_line
+#     after_comment
+#     before_tags
+#       tag_name
+#     after_tags
+#     feature_name
+#     before_feature_element
+#       before_tags
+#         tag_name
+#       after_tags
+#       scenario_name
+#       before_steps
+#         before_step
+#           before_step_result
+#             step_name
+#           after_step_result
+#         after_step
+#       after_steps
+#     after_feature_element
+#   after_feature
+# after_features
 module ProbeDockCucumber
   class Formatter
 
     def initialize step_mother, io, options
 
+      # Initialize the Probe Dock client and an empty test run.
       config = ProbeDockProbe.config
       @client = ProbeDockProbe::Client.new config.server, config.client_options
       @test_run = ProbeDockProbe::TestRun.new config.project
 
+      # Current feature data.
       @current_feature = nil
       @current_feature_tags = []
 
+      # Current scenario data.
       @current_scenario = nil
       @current_scenario_tags = []
       @current_scenario_error = nil
     end
 
+    # Called when the test suite starts.
     def before_features *args
-      @start_time = Time.now
+      @suite_start_time = Time.now
     end
 
+    # Called before each feature is tested.
     def before_feature feature, *args
+
+      # Store the first line of the feature's description.
+      # It will be used in #add_result to build the complete name of the test.
       @current_feature = feature.name.sub(/\n.*$/m, '').strip
+
+      # Reset feature and scenario data.
       @current_feature_tags = []
       @current_scenario = nil
+      @current_scenario_tags = []
+      @current_scenario_error = nil
     end
 
-    def after_tags tags, *args
+    # Called every time a tag is encountered, either at the feature or the scenario level.
+    def tag_name name, *args
       if @current_scenario
-        @current_scenario_tags = tags.tags.collect{ |tag| tag.name.sub(/^@/, '').strip }.uniq
+        @current_scenario_tags << name.sub(/^@/, '').strip
       else
-        @current_feature_tags = tags.tags.collect{ |tag| tag.name.sub(/^@/, '').strip }.uniq
+        @current_feature_tags << name.sub(/^@/, '').strip
       end
     end
 
+    # Called before each scenario is tested.
     def before_feature_element feature_element, *args
+
+      # Store the first line of the scenario's description.
+      # It will be used in #add_result to build the complete name of the test.
       @current_scenario = feature_element.name.sub(/\n.*$/m, '').strip
+
+      # Reset scenario data.
       @current_scenario_tags = []
       @current_scenario_error = nil
       @current_scenario_start_time = Time.now
     end
 
+    # Called after each scenario step (Given, When, Then) is executed.
     def after_step_result keyword, step_match, multiline_arg, status, exception, *args
-      if exception
-        @current_scenario_error = exception
-      end
+      # If a step fails, the exception is provided here.
+      # It will be used in #add_result to build the error message of the test.
+      @current_scenario_error = exception if exception
     end
 
+    # Called after each completed scenario.
     def after_feature_element *args
-      add_result !@current_scenario_error, @current_scenario_error
+      add_result
     end
 
+    # Called when the test suite ends.
     def after_features *args
-      #puts "after_features: #{args.inspect}"
       end_time = Time.now
-      @test_run.duration = ((end_time - @start_time) * 1000).round
-      puts @test_run.inspect
-      #@client.process @test_run
+      @test_run.duration = ((end_time - @suite_start_time) * 1000).round
+      @client.process @test_run
     end
 
     private
 
-    def add_result successful, error = nil
+    # Adds a result to the test run.
+    # The test and result data was stored while the features and scenarios
+    # were being executed (see #before_feature, #tag_name, etc).
+    def add_result
 
-      options = {
-        tags: (@current_feature_tags + @current_scenario_tags).uniq,
+      result_options = {
+        name: complete_name,
+        passed: !@current_scenario_error,
         data: {}
       }
+
+      result_options[:duration] = ((Time.now - @current_scenario_start_time) * 1000).round
+
+      # Combine the tags of the feature and of the scenario.
+      result_options[:tags] = (@current_feature_tags + @current_scenario_tags).uniq.sort
+
+      # The fingerprint identifying the test contains the first line of the
+      # feature's and the scenario's descriptions joined with a separator.
+      fingerprint_data = [ @current_feature, @current_scenario ]
+      result_options[:fingerprint] = Digest::SHA1.hexdigest fingerprint_data.join('|||')
+      result_options[:data][:fingerprint] = result_options[:fingerprint]
+
+      # Build the message from the error's message and backtrace if an error occurred.
+      result_options[:message] = failure_message @current_scenario_error if @current_scenario_error
+
+      @test_run.add_result result_options
+    end
+
+    # Builds the complete test name.
+    # The name is obtained by joining the first line of the feature's
+    # and the scenario's descriptions. Additionally, if the feature's
+    # description doesn't end with a dot, a semicolon is added
+    # (e.g. "Feature: Scenario").
+    def complete_name
 
       name = @current_feature.dup
       if name.match /\.$/
@@ -78,17 +154,6 @@ module ProbeDockCucumber
       end
 
       name << @current_scenario
-
-      options[:name] = name
-
-      fingerprint_data = [ @current_feature, @current_scenario ]
-      options[:fingerprint] = Digest::SHA1.hexdigest fingerprint_data.join('|||')
-      options[:data][:fingerprint] = options[:fingerprint]
-
-      options.merge! passed: successful, duration: ((Time.now - @current_scenario_start_time) * 1000).round
-      options[:message] = failure_message error if error
-
-      @test_run.add_result options
     end
 
     def failure_message error
---
+++ spec/formatter_spec.rb
@@ -0,0 +1,176 @@
+require 'helper'
+
+RSpec.describe ProbeDockCucumber::Formatter do
+  let(:server_double){ double }
+  let(:project_double){ double }
+  let(:client_options){ {} }
+  let(:config_double){ double server: server_double, project: project_double, client_options: client_options }
+  let(:test_run_double){ double :add_result => nil, :duration= => nil }
+  let(:client_double){ double process: nil }
+  let(:cucumber_step_mother_double){ double }
+  let(:cucumber_io_double){ double }
+  let(:cucumber_formatter_options){ {} }
+
+  before :each do
+    allow(ProbeDockProbe).to receive(:config).and_return(config_double)
+    allow(ProbeDockProbe::Client).to receive(:new).and_return(client_double)
+    allow(ProbeDockProbe::TestRun).to receive(:new).and_return(test_run_double)
+  end
+
+  describe "#initialize" do
+    it "should create a client and a test run" do
+      expect(ProbeDockProbe::Client).to receive(:new).with(server_double, client_options)
+      expect(ProbeDockProbe::TestRun).to receive(:new).with(project_double)
+      described_class.new cucumber_step_mother_double, cucumber_io_double, cucumber_formatter_options
+    end
+  end
+
+  describe "#add_result" do
+    let(:sample_fingerprint){ '8dcfaa683870181468dbeeb5c36f0e028105477f' }
+    let(:error_double){ double message: 'bug', backtrace: %w(a b c) }
+    subject{ described_class.new cucumber_step_mother_double, cucumber_io_double, cucumber_formatter_options }
+
+    let :features do
+      [
+        {
+          name: 'Probe Dock Cucumber',
+          scenarios: [
+            { name: 'It should work' }
+          ]
+        },
+        {
+          name: 'Probe Dock Cucumber',
+          tags: %w(a b c),
+          scenarios: [
+            { name: 'It should work', tags: %w(b d) },
+            { name: 'It might work', delay: 0.125 },
+            { name: 'It will not work', tags: %w(a foo e bar), error: true }
+          ]
+        },
+        {
+          name: 'Feature.',
+          scenarios: [
+            { name: 'Scenario' }
+          ]
+        }
+      ]
+    end
+
+    it "should work" do
+
+      expect_result_options({
+        name: 'Probe Dock Cucumber: It should work',
+        fingerprint: sample_fingerprint,
+        passed: true
+      })
+
+      expect_result_options({
+        name: 'Probe Dock Cucumber: It should work',
+        fingerprint: sample_fingerprint,
+        passed: true,
+        tags: %w(a b c d)
+      })
+
+      expect_result_options({
+        name: 'Probe Dock Cucumber: It might work',
+        fingerprint: 'd3579ae55325b799ee3bd7625c29b0dda21d7177',
+        passed: true,
+        tags: %w(a b c),
+        duration: 125
+      })
+
+      expect_result_options({
+        name: 'Probe Dock Cucumber: It will not work',
+        fingerprint: 'c7e98fe2379af012960ce5e31ae2e8b3ae215293',
+        passed: false,
+        message: "bug\n  # a\n  # b\n  # c",
+        tags: %w(a b bar c e foo)
+      })
+
+      expect_result_options({
+        name: 'Feature. Scenario',
+        fingerprint: 'fd4ca85610cbd90bbb08bd4d1c18f6c6a5f1eea8',
+        passed: true
+      })
+
+      expect(test_run_double).not_to receive(:add_result)
+
+      run
+    end
+
+    def expect_result_options expected = {}
+
+      expected[:name] ||= ''
+      expected[:fingerprint] ||= ''
+      expected[:key] ||= nil
+      expected[:passed] = true unless expected.key? :passed
+      expected[:tags] ||= []
+      expected[:data] ||= {}
+
+      expected[:tags].sort! if expected[:tags]
+      expected[:tickets].sort! if expected[:tickets]
+      expected[:data][:fingerprint] = expected[:fingerprint] if expected[:fingerprint]
+
+      expect(test_run_double).to receive(:add_result) do |result_options|
+
+        expect(result_options[:duration]).not_to be_nil
+
+        if expected[:duration]
+          expect(result_options[:duration]).to be >= expected[:duration]
+        else
+          expect(result_options[:duration]).to be >= 0
+          expect(result_options[:duration]).to be < 50
+        end
+
+        expected.delete :duration
+
+        expected.each_pair do |key,value|
+          expect(result_options[key]).to eq(value)
+        end
+
+        extra_keys = result_options.keys - expected.keys - [ :duration ]
+        expect(extra_keys).to be_empty, "did not expect the following keys: #{extra_keys.join(', ')}"
+      end
+    end
+
+    def run
+      subject.before_features
+
+      features.each do |feature_options|
+
+        feature = double name: feature_options[:name]
+        subject.before_feature feature
+
+        if feature_options[:tags]
+          feature_options[:tags].each do |tag|
+            subject.tag_name tag
+          end
+        end
+
+        if feature_options[:scenarios]
+          feature_options[:scenarios].each do |scenario_options|
+
+            scenario = double name: scenario_options[:name]
+            subject.before_feature_element scenario
+
+            if scenario_options[:tags]
+              scenario_options[:tags].each do |tag|
+                subject.tag_name tag
+              end
+            end
+
+            sleep scenario_options[:delay] if scenario_options[:delay]
+
+            if scenario_options[:error]
+              subject.after_step_result nil, nil, nil, nil, error_double
+            end
+
+            subject.after_feature_element scenario
+          end
+        end
+      end
+
+      subject.after_features
+    end
+  end
+end
